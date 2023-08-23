05e814f42ffaf9cc7c0aa37f74a70d6d942726f9
Leonardo Moreira <leonardomoreiravalerio@gmail.com>
2023-08-23 11:22:56 -0300

    dia 6 com desafio resolvido

---
+++ dia-6/script.js
@@ -0,0 +1,10 @@
+/*
+let tabuada = Number(prompt('qual tabuada vc deseja resolver? '))
+let limite = Number(prompt('até quanto vc deseja multiplicar? '))
+
+for (let i = 0; i <= limite; i++){
+    console.log(`${tabuada} * ${i} = ${tabuada * i}`)
+}
+*/
+
+
