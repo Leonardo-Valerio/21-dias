1a310a89a5080f29a0615e12e8b91ae9b3d19191
Leonardo Moreira <leonardomoreiravalerio@gmail.com>
2023-09-18 14:20:46 -0300

    desafio dia-18 realizado com sucesso, funções exibir, emprestimo e devolução estão todas funcionando agora

--- dia-18/ex1.js
+++ dia-18/ex1.js
@@ -25,30 +25,52 @@ class biblioteca{
         this.Telefone = telefone
         this.Acervo = []
     }
-    exibirInfos(nomeLivro){
-        for (let i = 0; i < this.Acervo.length; i++){
-            if (this.Acervo[i].Titulo == nomeLivro){
-                console.log(this.Acervo[i].Titulo)
-                console.log(this.Acervo[i].Autor)
-                console.log(this.Acervo[i].Ano)
-                console.log(this.Acervo[i].Disponibiladade)
-                return true
+    livroExiste(txt){
+       
+        while (true){
+            let nomeLivro = prompt(txt)
+            for (let i = 0 ; i < this.Acervo.length; i++){
+                if (this.Acervo[i].Titulo == nomeLivro){
+                    let indice = i
+                return indice
+                }
             }
+          
+            console.log('livro nao encontrado')
         }
-        return false
+        
+    }
+    exibirInfos(nomeLivro){
+        if( nomeLivro >= 0){
+            console.log(this.Acervo[nomeLivro].Titulo)
+            console.log(this.Acervo[nomeLivro].Autor)
+            console.log(this.Acervo[nomeLivro].Editora)
+            console.log(this.Acervo[nomeLivro].Ano)
+            console.log(this.Acervo[nomeLivro].Disponibiladade)
+        }
+        
        
     }
     emprestimo(nomeLivro){
-        for (let i = 0; i < this.Acervo.length; i++){
-            if (this.Acervo[i].Titulo == nomeLivro){
-                
-                return true
-            }
+        
+        if (this.Acervo[nomeLivro].Disponibiladade == true){
+            this.Acervo[nomeLivro].Disponibiladade = false 
+            console.log(this.Acervo[nomeLivro].Disponibiladade)
+            console.log('emprestimo realizado com sucesso')
+        }else{
+            console.log('livro nao disponivel para emprestimo')
         }
-        return false
-       
     }
-   
+    devolucao(nomeLivro){
+        if(this.Acervo[nomeLivro].Disponibiladade == false){
+            this.Acervo[nomeLivro].Disponibiladade = true
+            console.log(this.Acervo[nomeLivro].Disponibiladade)
+            console.log('livro devolvido com sucesso')
+        }
+        else{
+            console.log('livro já em estoque ou foi devolvido anteriormente')
+        }
+    }
 }
 const minhaBiblioteca = new biblioteca('Biblioteca Central', 'Rua ABC, 123', '1111-1111')
 
@@ -60,32 +82,12 @@ function exibir(){
     let livro = new livros(titulo,autor,editora,ano)
     arrayLivros.push(livro)
     minhaBiblioteca.Acervo.push(livro)
-    
-    
-
-    
 }
-function buscarLivros(txt){
-    let livro = prompt(txt)
-    while(!minhaBiblioteca.exibirInfos(livro)){
-        livro = prompt(txt)
-        if (livro){
-            minhaBiblioteca.exibirInfos(livro)
 
-            break
-        }
-        else{
-            console.log('livro nao encontrado')
-        }
-    }
-
-    
-
-}
 
 
 while (true){
-    let escolha = Number(prompt('digite a opção que deseja: 1 cadastrar livros, '))
+    let escolha = Number(prompt('digite a opção que deseja: 1 cadastrar livros, 2 para ver informações sobre os livros, 3 para fazer um emprestimo de livros, 4 para devolver livros e 5 para sair '))
     if (escolha == 1){
         exibir()
         console.log(arrayLivros)
@@ -93,9 +95,18 @@ while (true){
     }
     else if(escolha == 2){
 
-        buscarLivros('digite o livro que deseja ver as info: ')
+        let nomeLivro = minhaBiblioteca.livroExiste('Digite o nome do livro que deseja ver as informações: ');
+        minhaBiblioteca.exibirInfos(nomeLivro)
     }
     else if(escolha == 3){
+        let nomeLivro = minhaBiblioteca.livroExiste('Digite o nome do livro que deseja ver as informações: ');
+        minhaBiblioteca.emprestimo(nomeLivro)
+    }
+    else if(escolha == 4){
+        let nomeLivro = minhaBiblioteca.livroExiste('Digite o nome do livro que deseja ver as informações: ')
+        minhaBiblioteca.devolucao(nomeLivro)
+    }
+    else if(escolha == 5){
         break
     }
 }
